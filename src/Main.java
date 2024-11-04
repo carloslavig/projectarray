04590601b9fb96133cbcf0eea642f8515f994ede
carloslavig <carloslavig@gmail.com>
2024-11-03 21:23:05 -0300

    uptade 1.2

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,7 @@ public class Main
 {
     public static void main(String[] args)
     {
-        int numero1 =1;
-        int numero2 =2;
-        int numero3 =3;
+/*
 
         String [] letras =  new String[5];
         //[0][1][2][3][4]
@@ -13,6 +11,12 @@ public class Main
         letras[2]="C";
         letras[3]="D";
         letras[4]="E";
+        for (int i=0; i<letras.length;i++)
+        {
+            System.out.println(letras[i]);
+        }
+*/
+        String[] letras = {"A","B","C", "D", "E"};
         for (int i=0; i<letras.length;i++)
         {
             System.out.println(letras[i]);
