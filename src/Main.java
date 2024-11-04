ed04ca74944d1572ad081faf047d034397c34321
carloslavig <carloslavig@gmail.com>
2024-11-03 21:36:46 -0300

    update 1.2.5

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class Main
 {
     public static void main(String[] args)
@@ -17,10 +19,32 @@ public class Main
         }
 */
         String[] letras = {"A","B","C", "D", "E"};
-        for (int i=0; i<letras.length;i++)
+       /* for (int i=0; i<letras.length;i++)
         {
             System.out.println(letras[i]);
+        }*/
+//        System.out.println(Arrays.toString(letras));
+
+        int[] numeros = {9,10,12,25,2};
+        int maior = numeros[0];
+        int menor = numeros[0];
+        int media = 0;
+        for (int i=0;i< numeros.length;i++)
+        {
+            if (numeros[i]>maior)
+            {
+                maior=numeros[i];
+            }
+            if (numeros[i]<menor)
+            {
+                menor=numeros[i];
+            }
+            media += numeros[i];
         }
+
+        System.out.println("Maior: "+maior);
+        System.out.println("Menor: "+menor);
+        System.out.println("Média: "+media/ numeros.length);
 
     }
 }
